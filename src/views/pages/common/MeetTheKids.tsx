090bf21d5970d855298af5d6e4cb2e45abad43ff
Ryan Than <47007201+ryanthan@users.noreply.github.com>
2021-04-01 21:48:16 -0600

    Added simple descriptions for Meet the Kids

--- src/views/pages/common/MeetTheKids.tsx
+++ src/views/pages/common/MeetTheKids.tsx
@@ -7,7 +7,7 @@ import { CarouselPage } from '../../../components/Carousel';
 const pages: CarouselPage[] = [
     {
         description: 'High-maintenance children are emotionally reactive, which means they usually react strongly and negatively to stress or change. They often seem moody. They are low in task persistence and have difficulty finishing tasks. They also demonstrate high levels of movement and wiggle frequently.',
-        simpleDescription: 'Simple Description Needed',
+        simpleDescription: 'This is Gregory and Gretchen. They can be a bit grumpy sometimes but they both enjoy running around a lot and playing games with all of their friends! Say hello!',
         personalityType: 'High-Maintenance',
         images: [require('../../../assets/faceshots/Gregory.png'), require('../../../assets/faceshots/Gretchen.png')],
         names: ['Gregory', 'Gretchen'],
@@ -18,7 +18,7 @@ const pages: CarouselPage[] = [
     },
     {
         description: 'Industrious children demonstrate both high task persistence and low motor activity, which enables them to get things done. They take pleasure in their accomplishments. They are also low in negative reactivity, which means they are pleasant most of the time.',
-        simpleDescription: 'Simple Description Needed',
+        simpleDescription: 'This is Hilary and Henry. They are really nice and they both like working on fun activities. Say hello!',
         personalityType: 'Industrious',
         images: [require('../../../assets/faceshots/Hilary.png'), require('../../../assets/faceshots/Henry.png')],
         names: ['Hilary', 'Henry'],
@@ -29,7 +29,7 @@ const pages: CarouselPage[] = [
     },
     {
         description: 'Cautious children are often shy and slow to warm-up. They are low in approach, which means their first reaction is to withdraw from new or stressful situations. They also demonstrate high negative reactivity and will let you know when they are not pleased with a situation.',
-        simpleDescription: 'Simple Description Needed',
+        simpleDescription: 'This is Coretta and Carlos. They are a bit shy and quiet when you first meet them but they are really nice if you talk to them! Say hello!',
         personalityType: 'Shy & Cautious',
         images: [require('../../../assets/faceshots/Coretta.png'), require('../../../assets/faceshots/Carlos.png')],
         names: ['Coretta', 'Carlos'],
@@ -40,7 +40,7 @@ const pages: CarouselPage[] = [
     },
     {
         description: 'Social children are eager to meet new people. They are high in approach, often driven by a need to be with people and to try new experiences. Their temperaments are low in negative reactivity and withdrawal, which means they are usually pleasant most of the time.',
-        simpleDescription: 'Simple Description Needed',
+        simpleDescription: 'This is Fredrico and Felicity. They are both really nice and friendly! They like to make new friends and try new activities. Say hello!',
         personalityType: 'Social & Eager to Try',
         images: [require('../../../assets/faceshots/Fredrico.png'), require('../../../assets/faceshots/Felicity.png')],
         names: ['Fredrico', 'Felicity'],
