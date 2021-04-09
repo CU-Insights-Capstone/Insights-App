55c10df932320ec9ad79476fca0743e16b3a53bb
Will Ness <willness210@gmail.com>
2021-04-09 15:33:46 -0600

    finish gregory coloring game

--- src/components/color_games/GregoryColoringGame.tsx
+++ src/components/color_games/GregoryColoringGame.tsx
@@ -7,19 +7,53 @@ interface GregoryColoringGameProps {
 }
 
 const default_color = '#ffffff';
+/*
+interface GregorySVGProps {
+  hairColor: string;
+  faceColor: string;
+  backgroundColor1: string;
+  backgroundColor2: string;
+  shirtColor: string;
+  shirtCuffsColor: string;
+  handColor: string;
+  flagColor: string;
+  titleColor: string;
+}
 
+*/
 export const GregoryColoringGame = (props: GregoryColoringGameProps) => {
     const [hairColor, setHairColor] = React.useState(default_color);
     const [faceColor, setFaceColor] = React.useState(default_color);
+    const [backgroundColor1, setBackgroundColor1] = React.useState(default_color);
+    const [backgroundColor2, setBackgroundColor2] = React.useState(default_color);
+    const [shirtColor, setShirtColor] = React.useState(default_color);
+    const [shirtCuffsColor, setShirtCuffsColor] = React.useState(default_color);
+    const [handColor, setHandColor] = React.useState(default_color);
+    const [flagColor, setFlagColor] = React.useState(default_color);
+    const [titleColor, setTitleColor] = React.useState(default_color);
 
     return <SVGColorGame 
         componentSVG={<GregorySVG 
             hairColor={hairColor}
             faceColor={faceColor}
+            backgroundColor1={backgroundColor1}
+            backgroundColor2={backgroundColor2}
+            shirtColor={shirtColor}
+            shirtCuffsColor={shirtCuffsColor}
+            handColor={handColor}
+            flagColor={flagColor}
+            titleColor={titleColor}
         />}
         colorButtons={[
             {title: 'Hair', onClick: setHairColor},
             {title: 'Face', onClick: setFaceColor},
+            {title: 'Background', onClick: setBackgroundColor1},
+            {title: 'Background', onClick: setBackgroundColor2},
+            {title: 'Shirt', onClick: setShirtColor},
+            {title: 'Cuffs', onClick: setShirtCuffsColor},
+            {title: 'Hand', onClick: setHandColor},
+            {title: 'Flag', onClick: setFlagColor},
+            {title: 'Title', onClick: setTitleColor},
         ]}
     />
 }
